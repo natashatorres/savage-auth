ad87847a8f72a45411fb521d4939a3b4e9530861
Natasha Torres <natashaxtorres@gmail.com>
2021-10-26 16:58:28 -0400

    removed config from gitignore

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    'dbName': 'demo'
+};
+
+//this will export an object
